6d6bbc7e1408bf4924eea082d6b7edbfb5013810
Unknown <847719553@qq.com>
2018-06-15 13:27:02 +0800

    按时间顺序排列

--- db/articlesql.js
+++ db/articlesql.js
@@ -3,8 +3,8 @@ var ArticleSQL = {
     bangding:'UPDATE user SET userame = ?,password = ? WHERE userid=?  ',
     queryAll:'SELECT * FROM article order by articleId desc',
     queryFour:"SELECT * from article order by articleWriteDate desc limit 0,4",
-    queryPage:'SELECT * from article limit ?,?',
+    queryPage:'SELECT * from article limit order by articleWriteDate desc ?,?',
     queryByArticleId:'SELECT * FROM article WHERE articleId=?',
-    queryAllLimit:'SELECT * FROM  article limit ?,?'
+    queryAllLimit:'SELECT * FROM  article order by articleWriteDate desc limit ?,?'
 };
 module.exports = ArticleSQL;
